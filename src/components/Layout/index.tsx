52b7f1b7d19e023bf2d83074e04ced1831f5a535
Wanderson Paes <wandersonpaeswop3@gmail.com>
2021-02-09 17:58:41 -0300

    organizando pastas

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from './styles';
+
+const Layout: React.FC = () => {
+    return (
+        <Container>
+            <h1>TOT</h1>
+        </Container>
+    );
+}
+
+export default Layout;
